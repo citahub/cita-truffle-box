feb499f24a6d874344d439d4098daffffc13e375
Yao <GyonGyon@users.noreply.github.com>
2018-05-29 11:46:43 +0800

    update: remove migrate dir add default truffle-cita config

--- truffle-cita.js
+++ truffle-cita.js
@@ -1,4 +1,13 @@
 module.exports = {
-    networks: {},
-    contractInfo: {},
+    networks: {
+        development: {
+            host: '127.0.0.1', // your host
+            port: 1337,
+            network_id: '*',
+        },
+    },
+    contractInfo: {
+        chainId: 1,
+        privkey: null, // your private key
+    },
 }
