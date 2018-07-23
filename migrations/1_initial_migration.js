1d6de3430efd320c7621799736dea9c3ac36f35d
Yao <GyonGyon@users.noreply.github.com>
2018-07-23 19:13:27 +0800

    update: dependence version

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,11 @@
 var artifacts
 if (artifacts) {
-    var Migrations = artifacts.require('./HelloWorld.sol')
+  var Migrations = artifacts.require('./HelloWorld.sol')
 } else {
-    var Migrations = 'HelloWorld'
+  // this should be the name of contracts json file in dir ./build/contracts
+  var Migrations = 'HelloWorld'
 }
 
 module.exports = function(deployer) {
-    deployer.deploy(Migrations)
+  deployer.deploy(Migrations)
 }
