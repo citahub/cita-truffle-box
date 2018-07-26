2325d528a750ab2a54563cbddca9663b45163512
Yao <GyonGyon@users.noreply.github.com>
2018-07-26 10:44:56 +0800

    update: change config

--- truffle-cita.js
+++ truffle-cita.js
@@ -1,13 +1,13 @@
 module.exports = {
-    networks: {
-        development: {
-            host: '127.0.0.1', // your host
-            port: 1337,
-            network_id: '*',
-        },
+  networks: {
+    development: {
+      host: '127.0.0.1', // your host
+      port: 1337,
+      network_id: '*',
     },
-    contractInfo: {
-        chainId: 1,
-        privkey: null, // your private key
-    },
+  },
+  contractInfo: {
+    chainId: 1,
+    privateKey: null, // your private key
+  },
 }
