070a911a4f6077c0284e6cde5d05e895782f92df
Yao <GyonGyon@users.noreply.github.com>
2018-07-30 17:39:37 +0800

    update: migrations.sol
    add initial migrations.sol

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,14 @@
 var artifacts
 if (artifacts) {
-  var Migrations = artifacts.require('./HelloWorld.sol')
+  var Migrations = artifacts.require("Migrations");
 } else {
   // this should be the name of contracts json file in dir ./build/contracts
-  var Migrations = 'HelloWorld'
+  var Migrations = {
+    build: 'Migrations',
+  }
 }
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations)
-}
+module.exports = function (deployer) {
+  // Deploy the Migrations contract as our only task
+  deployer.deploy(Migrations);
+};
