a81c552b673af1673c4c5b00e24b47ea37d13e58
Yao <GyonGyon@users.noreply.github.com>
2018-08-06 14:31:10 +0800

    update: change migration scripts

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,14 +1,17 @@
 var artifacts
 if (artifacts) {
-  var Migrations = artifacts.require('Migrations')
+  var HelloWorld = artifacts.require('HelloWorld')
 } else {
   // this should be the name of contracts json file in dir ./build/contracts
+  var HelloWorld = {
+    build: 'HelloWorld',
+  }
   var Migrations = {
     build: 'Migrations',
   }
 }
 
 module.exports = function(deployer) {
-  // Deploy the Migrations contract as our only task
-  deployer.deploy(Migrations)
+  deployer.deploy([[HelloWorld], [Migrations]]).then((res) => {
+  })
 }
