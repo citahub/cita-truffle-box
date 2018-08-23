a7c18a432e98490d8e7198fcf7596936a961e0a1
Yao <yao@cryptape.com>
2018-08-23 11:47:28 +0800

    update: new-version-compatible-with-truffle

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,12 +1,4 @@
-var artifacts
-if (artifacts) {
-  var Migrations = artifacts.require('Migrations')
-} else {
-  // this should be the name of contracts json file in dir ./build/contracts
-  var Migrations = {
-    build: 'Migrations',
-  }
-}
+var Migrations = artifacts.require('Migrations')
 
 module.exports = function(deployer) {
   // Deploy the Migrations contract as our only task
--- migrations/2_helloworld.js
+++ migrations/2_helloworld.js
@@ -1,12 +1,4 @@
-var artifacts
-if (artifacts) {
-  var HelloWorld = artifacts.require('HelloWorld')
-} else {
-  // this should be the name of contracts json file in dir ./build/contracts
-  var HelloWorld = {
-    build: 'HelloWorld',
-  }
-}
+var HelloWorld = artifacts.require('HelloWorld')
 
 module.exports = function(deployer) {
   deployer.deploy([[HelloWorld, '测试']]).then((res) => {
