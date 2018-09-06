ed1c90712924cd121eee82d864c0b40728275716
Yao <yao@cryptape.com>
2018-09-06 10:32:51 +0800

    update: add how get hash and address in migration function

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,5 +2,8 @@ var Migrations = artifacts.require('Migrations')
 
 module.exports = function(deployer) {
   // Deploy the Migrations contract as our only task
-  deployer.deploy(Migrations)
+  deployer.deploy(Migrations).then((res) => {
+    console.log('hash', res.transactionHash)
+    console.log('address', res.address)
+  })
 }
