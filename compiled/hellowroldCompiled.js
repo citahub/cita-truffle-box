933e1a4c5a356a1d49880f023a3ce942f3a7e41a
Yao <yao@cryptape.com>
2018-09-26 19:04:29 +0800

    update: update and lock pkg version,

--- compiled/hellowroldCompiled.js
+++
@@ -1,4 +0,0 @@
-const address = "0x6bFfADB9AdD1Fc47631a26FBf1ad180ba303300C"
-const abi = [{"inputs":[{"name":"_name","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor","signature":"constructor"},{"constant":true,"inputs":[],"name":"sayHi","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x0c49c36c"},{"constant":false,"inputs":[{"name":"_name","type":"string"}],"name":"changeName","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x5353a2d8"}]
-
-export {address, abi}
